c6dd2ac2039adcdd0891d64a4fc931bffd1a089e
SujalShah3234 <shahc9437@gmail.com>
2021-07-13 09:26:24 +0530

    uncheck number indicator when dot indicator is unchecked

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ function toggleArrows(arrowCheckBox) {
 function toggleDots(dotsCheckBox) {
   let dots = document.querySelector(".carousel-dots");
   dots.style.display = dotsCheckBox.checked ? "block" : "none";
+  let toggleNumberIndicator = document.getElementById(
+    "toggle_number_indicator"
+  );
+  if (!dotsCheckBox.checked) {
+    toggleNumberIndicator.checked = false;
+  }
 }
 
 function toggleNumberIndicator(numberIndicatorCheckBox) {
@@ -31,5 +37,3 @@ function addCarousel() {
   document.querySelector(".carousel__container").innerHTML =
     "<img src='https://media.cntraveler.com/photos/5a8f3b070e2cf839e9dbfa1d/2:1/w_2560%2Cc_limit/NYC_GettyImages-640006562.jpg' alt='New York City Travel Guide &amp;amp; Tips | Condé Nast Traveler'>";
 }
-
-// TODO: Uncheck number indicator checkbox when Toggle indicator is unchecked.
